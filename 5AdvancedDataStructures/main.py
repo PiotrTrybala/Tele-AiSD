42e11c1f649fa0543646875943287567ba79f077
PiotrTrybala <trybalapiotr4@gmail.com>
2026-04-04 18:09:13 +0200

    save work on print

--- 5AdvancedDataStructures/main.py
+++ 5AdvancedDataStructures/main.py
@@ -1,24 +1,6 @@
-
+from collections import deque
 from queue import Queue
-
-
-def _level_traversal(node):
-    q = Queue()
-    q.put(node)
-    while not q.empty():
-        n = q.get()
-        print(n.value)
-        if n.left:
-            q.put(n.left)
-        if n.right:
-            q.put(n.right)
-
-def _inorder_traversal(node):
-    if not node:
-        return
-    _inorder_traversal(node.left)
-    print(node.value)
-    _inorder_traversal(node.right)
+from math import floor
 
 class BstNode:
 
@@ -58,12 +40,118 @@ class BstNode:
     def find(self, x):
         return self._find(self, x)
 
-
-    def inorder_traversal(self):
-        _inorder_traversal(self)
-
-    def level_traversal(self):
-        _level_traversal(self)
+    def _print(self, node, level=0, prefix=""):
 
 
+        print(f"(level={level}, value={node.value}, prefix_length = {len(prefix)}, prefix = '{prefix}')")
+        dashes = '-' * level
+        prefix = f'{prefix}{dashes}{node.value}'
+        print(prefix, end='')
 
+        if node.left:
+            node.left._print(node.left, level + 1, prefix)
+        else:
+            print()
+
+        if node.right:
+            spaces = ' ' * len(prefix)
+            print(f'spaces = {len(spaces)}')
+            new_prefix = f'{spaces}{node.value}'
+            # prefix = f'{spaces}{node.value}'
+            node.right._print(node.right, level + 1, new_prefix)
+
+
+    def print(self):
+        self._print(self)
+
+class ListNode:
+    def __init__(self, value):
+        self.next: ListNode | None = None
+        self.value: float = value
+        self.bst: BstNode = BstNode(value + 0.5)
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.node = ListNode(0.0)
+
+    def insert(self, x):
+        limit = floor(x)
+
+        cur = self.node
+        prev = None
+        while cur.next and cur.value < x:
+            # if prev:
+            #     print(f'x = {limit}, cur={cur.value} ({cur.value > x}, {cur.value == limit}); prev = {prev.value}')
+            # else:
+            #     print(f'cur = {cur.value}')
+
+            if cur.value == limit:
+                break
+            prev = cur
+            cur = cur.next
+
+        # print("\n")
+
+        if cur.value == limit:
+            cur.bst.insert(x)
+            return
+
+        new_node = ListNode(limit)
+        new_node.bst.insert(x)
+
+        if cur.next:
+            successor = cur
+            prev.next = new_node
+            prev.next.next = successor
+        else:
+            cur.next = new_node
+
+
+    def min(self, y):
+        pass
+
+    def max(self, y):
+        pass
+
+    def search(self, x):
+        pass
+
+    def print(self):
+        cur = self.node
+        while cur.next:
+            print(f'{cur.value} ', end="-> ")
+            cur = cur.next
+
+        print("None")
+
+# l = LinkedList()
+#
+# l.insert(1.3)
+# l.insert(9.3)
+# l.insert(1.6)
+# l.insert(4.99)
+# l.insert(7.8)
+# l.insert(3.7)
+# l.insert(4.0)
+# l.insert(7.9)
+# l.insert(7.6)
+# l.insert(7.3)
+# l.insert(7.7)
+#
+# l.print()
+
+bst = BstNode(4)
+bst.insert(2)
+# bst.insert(6)
+bst.insert(1)
+bst.insert(3)
+# bst.insert(5)
+# bst.insert(7)
+
+
+# bst.inorder_traversal()
+# bst.print()
+
+bst.print()
